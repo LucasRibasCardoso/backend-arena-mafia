2d33ea1261344403d1c4c7f6627115a55c8e4231
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-10 11:19:06 -0300

    build: initialize Spring Boot project with Maven setup and basic configuration

---
+++ src/main/java/com/projetoExtensao/arenaMafia/ArenaMafiaApplication.java
@@ -0,0 +1,12 @@
+package com.projetoExtensao.arenaMafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaMafiaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArenaMafiaApplication.class, args);
+  }
+}

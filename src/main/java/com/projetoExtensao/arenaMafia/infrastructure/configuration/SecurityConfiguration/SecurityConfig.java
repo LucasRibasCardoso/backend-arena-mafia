1216bb6a20ab347ba821e7ab2d7a00ff32aa2719
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-10 11:38:46 -0300

    feat: atualizar configuração de segurança para permitir opções de cabeçalho e desabilitar login padrão

--- src/main/java/com/projetoExtensao/arenaMafia/infrastructure/configuration/SecurityConfiguration/SecurityConfig.java
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/configuration/SecurityConfiguration/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,10 +18,15 @@ public class SecurityConfig {
 
     // Desabilita configurações padrão de segurança
     http
+        .headers(headers -> headers
+            .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+        )
         .csrf(CsrfConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
-        );
+        )
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable);
     return http.build();
   }
 }

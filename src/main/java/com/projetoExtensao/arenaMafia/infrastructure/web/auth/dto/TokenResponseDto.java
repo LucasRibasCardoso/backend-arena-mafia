45d9a52dc3b149cddb0f8fa385d666454cbe9de9
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-22 11:59:43 -0300

    refactor: remover os campos desnecessários para o TokenResponseDto

--- src/main/java/com/projetoExtensao/arenaMafia/infrastructure/web/auth/dto/TokenResponseDto.java
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/web/auth/dto/TokenResponseDto.java
@@ -1,10 +1,3 @@
 package com.projetoExtensao.arenaMafia.infrastructure.web.auth.dto;
 
-import java.time.LocalDateTime;
-
-public record TokenResponseDto(
-    String username,
-    LocalDateTime createdAt,
-    LocalDateTime expiresAt,
-    String accessToken,
-    String refreshToken) {}
+public record TokenResponseDto(String username, String accessToken) {}

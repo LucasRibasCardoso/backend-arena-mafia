348e4b81f633b958b182317569fb3a8ecf3cc975
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-22 11:34:20 -0300

    feat: adicionar @transactional aos métodos de UserRepositoryAdapter

--- src/main/java/com/projetoExtensao/arenaMafia/infrastructure/adapter/UserRepositoryAdapter.java
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/adapter/UserRepositoryAdapter.java
@@ -1,13 +1,14 @@
 package com.projetoExtensao.arenaMafia.infrastructure.adapter;
 
-import com.projetoExtensao.arenaMafia.application.repository.UserRepositoryPort;
+import com.projetoExtensao.arenaMafia.application.port.repository.UserRepositoryPort;
 import com.projetoExtensao.arenaMafia.domain.model.User;
 import com.projetoExtensao.arenaMafia.infrastructure.persistence.entity.UserEntity;
-import com.projetoExtensao.arenaMafia.infrastructure.persistence.mapper.UserMapper;
 import com.projetoExtensao.arenaMafia.infrastructure.persistence.repository.UserJpaRepository;
+import com.projetoExtensao.arenaMafia.infrastructure.persistence.mapper.UserMapper;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class UserRepositoryAdapter implements UserRepositoryPort {
@@ -21,26 +22,31 @@ public class UserRepositoryAdapter implements UserRepositoryPort {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public boolean existsByPhone(String phone) {
     return userJpaRepository.existsByPhone(phone);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public boolean existsByUsername(String username) {
     return userJpaRepository.existsByUsername(username);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Optional<User> findById(UUID id) {
     return userJpaRepository.findById(id).map(userMapper::toDomain);
   }
 
   @Override
+  @Transactional(readOnly = true)
   public Optional<User> findByUsername(String username) {
     return userJpaRepository.findByUsername(username).map(userMapper::toDomain);
   }
 
   @Override
+  @Transactional
   public User save(User user) {
     UserEntity userEntity = userMapper.toEntity(user);
     UserEntity savedUserEntity = userJpaRepository.save(userEntity);

720c3736111508e71641f9d0e2090a47ee03adcb
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-23 00:11:19 -0300

    refactor: organizar imports e formatação do código

--- src/main/java/com/projetoExtensao/arenaMafia/domain/exception/refreshToken/RefreshTokenNotFoundException.java
+++ src/main/java/com/projetoExtensao/arenaMafia/domain/exception/refreshToken/RefreshTokenNotFoundException.java
@@ -1,6 +1,5 @@
 package com.projetoExtensao.arenaMafia.domain.exception.refreshToken;
 
-
 public class RefreshTokenNotFoundException extends BadRefreshTokenException {
   public RefreshTokenNotFoundException(String message) {
     super(message);
--- src/main/java/com/projetoExtensao/arenaMafia/infrastructure/config/beans/UseCaseBeans.java
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/config/beans/UseCaseBeans.java
@@ -2,10 +2,10 @@ package com.projetoExtensao.arenaMafia.infrastructure.config.beans;
 
 import com.projetoExtensao.arenaMafia.application.port.gateway.AuthPort;
 import com.projetoExtensao.arenaMafia.application.port.repository.RefreshTokenRepositoryPort;
-import com.projetoExtensao.arenaMafia.application.useCase.implementation.LoginUseCaseImp;
-import com.projetoExtensao.arenaMafia.application.useCase.implementation.RefreshTokenUseCaseImp;
 import com.projetoExtensao.arenaMafia.application.useCase.LoginUseCase;
 import com.projetoExtensao.arenaMafia.application.useCase.RefreshTokenUseCase;
+import com.projetoExtensao.arenaMafia.application.useCase.implementation.LoginUseCaseImp;
+import com.projetoExtensao.arenaMafia.application.useCase.implementation.RefreshTokenUseCaseImp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 

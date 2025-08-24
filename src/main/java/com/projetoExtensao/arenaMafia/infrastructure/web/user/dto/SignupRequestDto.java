492832a7a3e4407766684ad790c73e9ca0def8a9
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-23 22:55:03 -0300

    feat: criar DTOs para requisição e resposta de cadastro de usuário

---
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/web/user/dto/SignupRequestDto.java
@@ -0,0 +1,28 @@
+package com.projetoExtensao.arenaMafia.infrastructure.web.user.dto;
+
+import com.projetoExtensao.arenaMafia.infrastructure.web.user.annotation.PasswordsMatch;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@PasswordsMatch(message = "A senha e a confirmação de senha não conferem.")
+public record SignupRequestDto(
+
+    @NotBlank(message = "Por favor, informe um nome de usuário.")
+    @Pattern(regexp = "^[a-zA-Z0-9_]{3,20}$", message = "O nome de usuário deve conter apenas letras, números e underscore (_).")
+    @Size(min = 3, max = 20, message = "O nome de usuário deve ter entre 3 e 20 caracteres.")
+    String username,
+
+    @NotBlank(message = "Por favor, informe seu nome completo.")
+    String fullName,
+
+    @NotBlank(message = "Por favor, informe seu telefone.")
+    @Pattern(regexp = "^\\+\\d{10,15}$", message = "O telefone deve estar no formato internacional (ex: +5547999999999).")
+    String phone,
+
+    @NotBlank(message = "Por favor, insira uma senha.")
+    @Size(min = 6, max = 20, message = "A senha deve ter entre 6 e 20 caracteres.")
+    String password,
+
+    // Não precisa de annotation, pois a sua comparação é feita pela annotation @PasswordsMatch
+    String confirmPassword) {}
---
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/web/user/dto/SignupResponseDto.java
@@ -0,0 +1,3 @@
+package com.projetoExtensao.arenaMafia.infrastructure.web.user.dto;
+
+public record SignupResponseDto(String status, String message, String identifier) {}

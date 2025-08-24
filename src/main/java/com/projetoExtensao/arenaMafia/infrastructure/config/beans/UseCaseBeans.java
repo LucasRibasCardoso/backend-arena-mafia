60d16eab9493a1a70aecc710c08cd062dac4510a
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-08-24 01:56:44 -0300

    feat: adicionar suporte ao SignUpUseCase e atualizar LoginUseCase com UserRepositoryPort

--- src/main/java/com/projetoExtensao/arenaMafia/infrastructure/config/beans/UseCaseBeans.java
+++ src/main/java/com/projetoExtensao/arenaMafia/infrastructure/config/beans/UseCaseBeans.java
@@ -2,10 +2,13 @@ package com.projetoExtensao.arenaMafia.infrastructure.config.beans;
 
 import com.projetoExtensao.arenaMafia.application.port.gateway.auth.AuthPort;
 import com.projetoExtensao.arenaMafia.application.port.repository.RefreshTokenRepositoryPort;
+import com.projetoExtensao.arenaMafia.application.port.repository.UserRepositoryPort;
 import com.projetoExtensao.arenaMafia.application.useCase.LoginUseCase;
 import com.projetoExtensao.arenaMafia.application.useCase.RefreshTokenUseCase;
+import com.projetoExtensao.arenaMafia.application.useCase.SignUpUseCase;
 import com.projetoExtensao.arenaMafia.application.useCase.implementation.LoginUseCaseImp;
 import com.projetoExtensao.arenaMafia.application.useCase.implementation.RefreshTokenUseCaseImp;
+import com.projetoExtensao.arenaMafia.application.useCase.implementation.SignUpUseCaseImp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +16,13 @@ import org.springframework.context.annotation.Configuration;
 public class UseCaseBeans {
 
   @Bean
-  public LoginUseCase loginUseCase(AuthPort authPort) {
-    return new LoginUseCaseImp(authPort);
+  public LoginUseCase loginUseCase(AuthPort authPort, UserRepositoryPort userRepository) {
+    return new LoginUseCaseImp(authPort, userRepository);
+  }
+
+  @Bean
+  public SignUpUseCase signUpUseCase() {
+    return new SignUpUseCaseImp();
   }
 
   @Bean

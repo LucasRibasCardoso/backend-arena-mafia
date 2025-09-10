8d7eeb167de212c503645cfc2007f25c04604cd5
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-09-10 00:28:47 -0300

    test(security): adicionar testes de integração para status de conta inválido

--- src/test/java/com/projetoExtensao/arenaMafia/integration/security/SecurityIntegrationTest.java
+++ src/test/java/com/projetoExtensao/arenaMafia/integration/security/SecurityIntegrationTest.java
@@ -3,7 +3,10 @@ package com.projetoExtensao.arenaMafia.integration.security;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.projetoExtensao.arenaMafia.application.user.port.gateway.PendingPhoneChangePort;
+import com.projetoExtensao.arenaMafia.domain.exception.ErrorCode;
 import com.projetoExtensao.arenaMafia.domain.model.User;
+import com.projetoExtensao.arenaMafia.domain.model.enums.AccountStatus;
 import com.projetoExtensao.arenaMafia.infrastructure.web.exception.dto.ErrorResponseDto;
 import com.projetoExtensao.arenaMafia.infrastructure.web.user.dto.request.UpdateProfileRequestDto;
 import com.projetoExtensao.arenaMafia.integration.config.WebIntegrationTestConfig;
@@ -11,13 +14,17 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @DisplayName("Testes de Integração para a Camada de Segurança")
 public class SecurityIntegrationTest extends WebIntegrationTestConfig {
+
+  @Autowired private PendingPhoneChangePort pendingPhoneChangePort;
 
   private RequestSpecification specification;
 
@@ -29,6 +36,105 @@ public class SecurityIntegrationTest extends WebIntegrationTestConfig {
             .setBasePath("/api/users/me")
             .setContentType(MediaType.APPLICATION_JSON_VALUE)
             .build();
+  }
+
+  @Nested
+  @DisplayName("Deve retornar 401 Unauthorized quando o status conta for inválido")
+  class AccountStatusTests {
+    @Test
+    @DisplayName("Deve retornar 401 Unauthorized quando a conta está desativada")
+    void resendChangePhoneOtp_shouldReturn401_whenUserAccountIsDisabled() {
+      // Arrange
+      User mockUser = mockPersistUser();
+      AuthTokensTest tokens = mockLogin(defaultUsername, defaultPassword);
+      alterAccountStatus(mockUser.getId(), AccountStatus.DISABLED);
+
+      String newPhone = "+5547992044567";
+      pendingPhoneChangePort.save(mockUser.getId(), newPhone);
+
+      var response =
+          given()
+              .spec(specification)
+              .header("Authorization", "Bearer " + tokens.accessToken())
+              .when()
+              .post("/phone/verification/resend")
+              .then()
+              .log()
+              .all()
+              .statusCode(401)
+              .extract()
+              .as(ErrorResponseDto.class);
+
+      // Assert
+      ErrorCode errorCode = ErrorCode.ACCOUNT_DISABLED;
+
+      assertThat(response.status()).isEqualTo(401);
+      assertThat(response.path()).isEqualTo("/api/users/me/phone/verification/resend");
+      assertThat(response.errorCode()).isEqualTo(errorCode.name());
+      assertThat(response.developerMessage()).isEqualTo(errorCode.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve retornar 401 Unauthorized quando a conta está bloqueada")
+    void resendChangePhoneOtp_shouldReturn401_whenUserAccountIsLocked() {
+      // Arrange
+      User mockUser = mockPersistUser();
+      AuthTokensTest tokens = mockLogin(defaultUsername, defaultPassword);
+      alterAccountStatus(mockUser.getId(), AccountStatus.LOCKED);
+
+      String newPhone = "+5547992044567";
+      pendingPhoneChangePort.save(mockUser.getId(), newPhone);
+
+      var response =
+          given()
+              .spec(specification)
+              .header("Authorization", "Bearer " + tokens.accessToken())
+              .when()
+              .post("/phone/verification/resend")
+              .then()
+              .statusCode(401)
+              .extract()
+              .as(ErrorResponseDto.class);
+
+      // Assert
+      ErrorCode errorCode = ErrorCode.ACCOUNT_LOCKED;
+
+      assertThat(response.status()).isEqualTo(401);
+      assertThat(response.path()).isEqualTo("/api/users/me/phone/verification/resend");
+      assertThat(response.errorCode()).isEqualTo(errorCode.name());
+      assertThat(response.developerMessage()).isEqualTo(errorCode.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve retornar 401 Unauthorized quando a conta está pendente de verificação")
+    void resendChangePhoneOtp_shouldReturn401_whenUserAccountIsPendingVerification() {
+      // Arrange
+      User mockUser = mockPersistUser();
+      AuthTokensTest tokens = mockLogin(defaultUsername, defaultPassword);
+      alterAccountStatus(mockUser.getId(), AccountStatus.PENDING_VERIFICATION);
+
+      String newPhone = "+5547992044567";
+      pendingPhoneChangePort.save(mockUser.getId(), newPhone);
+
+      var response =
+          given()
+              .spec(specification)
+              .header("Authorization", "Bearer " + tokens.accessToken())
+              .when()
+              .post("/phone/verification/resend")
+              .then()
+              .statusCode(401)
+              .extract()
+              .as(ErrorResponseDto.class);
+
+      // Assert
+      ErrorCode errorCode = ErrorCode.ACCOUNT_PENDING_VERIFICATION;
+
+      assertThat(response.status()).isEqualTo(401);
+      assertThat(response.path()).isEqualTo("/api/users/me/phone/verification/resend");
+      assertThat(response.errorCode()).isEqualTo(errorCode.name());
+      assertThat(response.developerMessage()).isEqualTo(errorCode.getMessage());
+    }
   }
 
   @Test
@@ -45,8 +151,12 @@ public class SecurityIntegrationTest extends WebIntegrationTestConfig {
             .extract()
             .as(ErrorResponseDto.class);
 
-    assertThat(response.message())
-        .isEqualTo("Sua sessão é inválida ou expirou. Por favor, faça login novamente.");
+    ErrorCode errorCode = ErrorCode.SESSION_EXPIRED;
+
+    assertThat(response.status()).isEqualTo(401);
+    assertThat(response.path()).isEqualTo("/api/users/me/profile");
+    assertThat(response.errorCode()).isEqualTo(errorCode.name());
+    assertThat(response.developerMessage()).isEqualTo(errorCode.getMessage());
   }
 
   @Test
@@ -68,11 +178,17 @@ public class SecurityIntegrationTest extends WebIntegrationTestConfig {
             .when()
             .patch("/profile")
             .then()
+            .log()
+            .all()
             .statusCode(401)
             .extract()
             .as(ErrorResponseDto.class);
 
-    assertThat(response.message())
-        .isEqualTo("Sua sessão é inválida ou expirou. Por favor, faça login novamente.");
+    ErrorCode errorCode = ErrorCode.SESSION_EXPIRED;
+
+    assertThat(response.status()).isEqualTo(401);
+    assertThat(response.path()).isEqualTo("/api/users/me/profile");
+    assertThat(response.errorCode()).isEqualTo(errorCode.name());
+    assertThat(response.developerMessage()).isEqualTo(errorCode.getMessage());
   }
 }
